98dbc1230fdbd72d5f61e8db08e972906cd17981
EC2 Default User <ec2-user@ip-172-31-43-95.ap-northeast-1.compute.internal>
2024-09-16 12:11:53 +0000

    admin会員情報編集作成中

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,32 @@
 class Admin::CustomersController < ApplicationController
-  
+
   def index
     @customers = Customer.all.page(params[:page]).per(10)
-  end 
-  
+  end
+
   def show
     @customer = Customer.find(params[:id])
-  end 
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      # 変更完了後の遷移先は適宜変更
+      redirect_to edit_admin_customer_path, notice: "会員情報が更新"
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana,
+    :first_name_kana, :post_code, :address, :tellphone_number, :email, :is_active)
+  end
+
 end
