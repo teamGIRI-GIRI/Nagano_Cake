bb7d49822f1abae1af88ec2a4b06eb822b93004c
EC2 Default User <ec2-user@ip-172-31-43-95.ap-northeast-1.compute.internal>
2024-09-16 06:40:50 +0000

    レイアウト修正中

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,7 +1,7 @@
 class Admin::CustomersController < ApplicationController
   
   def index
-    @customers = Customer.all.page(params[:page])
+    @customers = Customer.all.page(params[:page]).per(10)
   end 
   
 end
