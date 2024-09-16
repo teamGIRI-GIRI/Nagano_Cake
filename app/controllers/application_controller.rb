5ac777fe2a5ccc20ec9a8761e4e0f66353a4f5bc
EC2 Default User <ec2-user@ip-172-31-19-10.ap-northeast-1.compute.internal>
2024-09-16 07:57:24 +0000

    seedsに管理者情報記入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ before_action :authenticate_admin!
 before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    admin_path
+    admin_top_path
   end
 
   def after_sign_out_path_for(resource)
@@ -11,7 +11,7 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
 end
