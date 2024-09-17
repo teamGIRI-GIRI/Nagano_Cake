f7150d6ae3a29f976d899793d87bf95dd365306c
EC2 Default User <ec2-user@ip-172-31-43-95.ap-northeast-1.compute.internal>
2024-09-17 13:25:15 +0000

    public商品一覧作成中

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -18,6 +18,9 @@ class Admin::SessionsController < Devise::SessionsController
   #   super
   # end
 
+def after_sign_in_path_for(resource)
+  admin_top_path
+end
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
