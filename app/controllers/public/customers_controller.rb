b5cabb21467d0765f20704313f0909a1ba684f32
EC2 Default User <ec2-user@ip-172-31-19-10.ap-northeast-1.compute.internal>
2024-09-18 09:44:37 +0000

    会員情報編集、退会画面編集

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -11,7 +11,20 @@ class Public::CustomersController < ApplicationController
   
   def update
     @customer = current_customer
-    @customer.update
+    if @customer.update(customer_params)
+       flash[:success] = "登録情報を変更しました。"
+       redirect_to customers_show_path
+    else
+        render "edit"
+    end
   end
+  
+  def withdraw
+    @customer = current_customer
+    @customer.update(is_active: true)
+    reset_session
+    
+    redirect_to root_path
+  end 
   
 end
