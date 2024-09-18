018bfb0394f519c579c6702d1f8e1caf8f9147d8
EC2 Default User <ec2-user@ip-172-31-19-10.ap-northeast-1.compute.internal>
2024-09-18 03:48:13 +0000

    退会確認画面作成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,7 +1,17 @@
 class Public::CustomersController < ApplicationController
-  
+  before_action :authenticate_customer!
+
   def show
+    @customer = current_customer
   end 
   
+  def edit
+    @customer = current_customer
+  end 
+  
+  def update
+    @customer = current_customer
+    @customer.update
+  end
   
 end
