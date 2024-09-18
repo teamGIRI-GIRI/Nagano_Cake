5ca60b7c80869730a8fb2e6171c1f2a7026adf24
EC2 Default User <ec2-user@ip-172-31-43-95.ap-northeast-1.compute.internal>
2024-09-18 10:35:58 +0000

    admin/index,show,edit調整中

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -16,7 +16,7 @@ class Admin::CustomersController < ApplicationController
     @customer = Customer.find(params[:id])
     if @customer.update(customer_params)
       # 変更完了後は会員詳細画面に遷移
-      redirect_to admin_customer_path, notice: "会員情報が更新されました。"
+      redirect_to admin_customer_path(@customer), notice: "会員情報が更新されました。"
     else
       render "edit"
     end
@@ -26,7 +26,7 @@ class Admin::CustomersController < ApplicationController
 
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana,
-    :first_name_kana, :post_code, :address, :tellphone_number, :email, :is_active)
+    :first_name_kana, :postal_code, :address, :tellphone_number, :email, :is_active)
   end
 
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,11 +3,11 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :orders
   has_many :cart_items
   has_many :addresses
-         
+
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
@@ -16,8 +16,8 @@ class Customer < ApplicationRecord
   validates :address, presence: true
   validates :tellphone_number, presence: true
   validates :email, presence: true
-  validates :is_avtive, presence: true
-  
+  validates :is_active, presence: true
+
 
 
 end
