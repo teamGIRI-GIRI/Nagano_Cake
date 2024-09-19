166b90d721274e9ffde235b7861592c1b462c1e8
EC2 Default User <ec2-user@ip-172-31-43-95.ap-northeast-1.compute.internal>
2024-09-19 11:16:54 +0000

    ショッピングカート画面商品一覧表作成中

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,6 +1,27 @@
 class Public::CartItemsController < ApplicationController
 
-  def create
+  def index
+    # どの会員のた―都内アイテムか限定する必要がある
+    @cart_items = CartItem.all
   end
+
+  def create
+    @cart_item = CartItem.new(cart_item_params)
+    # @cart_item.customer_id = current_customer.id
+    if @cart_item.save
+        redirect_to cart_items_path
+    else
+      redirect_to root_path
+    end
+  end
+
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:amount, :item_id, :customer_id)
+  end
+
+
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -20,5 +20,9 @@ class Item < ApplicationRecord
     end
     item_image.variant(resize_to_limit: [width, height]).processed
   end
+  
+  def with_tax_price
+    (price * 1.1).floor
+  end
 end
 
