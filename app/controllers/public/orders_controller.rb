3f67a15b46e2aa36092be7a5fcbe362d30f2fea4
Yuka511 <174612970+Yuka511@users.noreply.github.com>
2024-09-20 09:14:17 +0000

    customerの注文履歴一覧・注文履歴詳細[途中]

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,10 +4,10 @@ class Public::OrdersController < ApplicationController
   end
   
   def index
-    @orders = Order.all
+    @orders = current_customer.orders
   end
   
   def show
-    
+    @order = current_customer.orders.find(params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -72,9 +72,18 @@ ActiveRecord::Schema.define(version: 2024_09_17_132403) do
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "password_confirmation", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "last_name", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "last_name_kana", default: "", null: false
+    t.string "first_name_kana", default: "", null: false
+    t.string "postal_code", default: "", null: false
+    t.string "address", default: "", null: false
+    t.string "tellphone_number", default: "", null: false
+    t.boolean "is_active", default: true, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
