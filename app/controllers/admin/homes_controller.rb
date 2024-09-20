f7dd7979acbf1d3469722b33413384cc7d8dd6b3
EC2 Default User <ec2-user@ip-172-31-19-10.ap-northeast-1.compute.internal>
2024-09-20 04:03:51 +0000

    admin 注文履歴一覧編集

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,10 @@
 class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
   
+  def top
+    @orders = Order.page(params[:page]).per(3).order(created_at: :desc).limit(10)
+  
+  end
+  
+  
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,8 +1,7 @@
 class Admin::OrdersController < ApplicationController
     
   def index
-    @customer = Customer.find(params[:customer_id])
-    @orders = @customer.orders
+    
   end 
 
 end
