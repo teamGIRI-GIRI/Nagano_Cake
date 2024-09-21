b14693400a0625f072e34e193714d420835ec25e
EC2 Default User <ec2-user@ip-172-31-19-10.ap-northeast-1.compute.internal>
2024-09-21 14:02:08 +0000

    admin 注文履歴詳細、注文一覧編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Admin.create!(
-  email: "girigiri@mail",
-  password: "girigiri"
+#Admin.create!(
+#  email: "girigiri@mail",
+#  password: "girigiri"
+#  )
+  
+Order.create!(
+  customer_id: 1,
+  postal_code: "1234567",
+  address: "北海道",
+  name: "安田",
+  shipping_cost: 1000,
+  total_payment: 2000,
+  payment_method: 1,
+  status: 0,
   )
