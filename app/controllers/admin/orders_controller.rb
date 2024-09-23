361a5cab4d3d11ed1e5c99130a08849502ced221
EC2 Default User <ec2-user@ip-172-31-19-10.ap-northeast-1.compute.internal>
2024-09-23 08:15:36 +0000

    public ログイン画面レイアウト編集

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -4,15 +4,14 @@ class Admin::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @orders = Order.all
     @order_details = OrderDetail.where(order_id: @order.id)
-
   end
 
   def update
     @order = Order.find(params[:id])
-    @order_details = OrderDetail.where(order_id: @order)
+    @order_details = OrderDetail.where(order_id: @order.id)
     if @order.update(status_params)
       if @order.status.include?("入金確認")
-         @order_details.update( making_status: 1)
+        @order_details.update( making_status: 1)
       end
     flash[:success] = "制作ステータスを変更しました。"
     redirect_to admin_order_path(@order)
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,6 @@ class Order < ApplicationRecord
   has_many :order_details
   
   enum payment_method: { credit_card: 0, transfer: 1 }
-  
   enum status: {waiting: 0, paied: 1, making: 2, ready_to_ship: 3, shipped: 4}
   
   def address_display
