3fe38c9be6809b4943ec5555c27e6b3378c4319e
Yuka511 <174612970+Yuka511@users.noreply.github.com>
2024-09-23 03:22:51 +0000

    配送先一覧のcurrent_customerのみの表示

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,41 +1,43 @@
 class Public::AddressesController < ApplicationController
   before_action :authenticate_customer!
-  
+
   def index
     @address = Address.new
-    @addresses = Address.all
+    @addresses = current_customer.addresses
   end
-  
+
   def create
     @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
     if @address.save
       redirect_to addresses_path
     else
-      @address = Address.all
+      Rails.logger.error @address.errors.full_messages.join(", ") # エラーメッセージをログに出力
+      @addresses = Address.all
       render :index
     end
   end
-  
+
   def edit
     @address = Address.find(params[:id])
   end
-  
+
   def update
     @address = Address.find(params[:id])
     @address.update(address_params)
     redirect_to addresses_path
   end
-  
+
   def destroy
     @address = Address.find(params[:id])
     @address.destroy
     redirect_to addresses_path
   end
-  
+
   private
   #ストロングパラメータ
   def address_params
     params.require(:address).permit(:postal_code, :address, :name)
   end
-  
+
 end
