16a02b2259e61b873af3289b79941ef7162487ee
Mo-keke <175957589+Mo-keke@users.noreply.github.com>
2024-09-25 20:03:59 +0000

    Some Update

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -12,27 +12,33 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
-    select_address = params[:order][:select_address]
-    if (select_address == "2") && ((params[:order][:postal_code].empty?) || (params[:order][:address].empty?) || (params[:order][:name].empty?))
-      flash[:danger] = "住所を入力してください。"
-      redirect_to new_order_path
-    end
     @order = Order.new(order_params)
     @cart_items = CartItem.where(customer_id: current_customer.id)
     @order.payment_method = Order.payment_methods[params[:order][:payment_method]]
     @order.shipping_cost = 800
     @subtotal_sum = @cart_items.sum { |cart_item| cart_item.subtotal }
     @order.total_payment = @order.shipping_cost + @subtotal_sum
+    select_address = params[:order][:select_address]
     case select_address
       when "0" then
         @order.postal_code = current_customer.postal_code
         @order.address = current_customer.address
         @order.name = current_customer.last_name + current_customer.first_name
       when "1" then
-        register_address = Address.find(params[:order][:address_id])
-        @order.postal_code = register_address.postal_code
-        @order.address = register_address.address
-        @order.name = register_address.name
+        if params[:order][:address_id].nil?
+          flash[:danger] = "住所が選択されていません。"
+          redirect_to new_order_path
+        else
+          register_address = Address.find(params[:order][:address_id])
+          @order.postal_code = register_address.postal_code
+          @order.address = register_address.address
+          @order.name = register_address.name
+        end
+      when "2" then
+        if (params[:order][:postal_code].empty?) || (params[:order][:address].empty?) || (params[:order][:name].empty?)
+          flash[:danger] = "住所を入力してください。"
+          redirect_to new_order_path
+        end
     end
   end
 
