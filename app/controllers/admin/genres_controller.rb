3ec6d0372d4ec7eebe12854684a4cc15cb91f72f
Yuka511 <174612970+Yuka511@users.noreply.github.com>
2024-09-25 02:45:14 +0000

    admin/ジャンル登録バリデーション（エラーメッセージ表示）追加

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -11,7 +11,7 @@ class Admin::GenresController < ApplicationController
     if @genre.save
       redirect_to admin_genres_path
     else
-      @genres = Book.all
+      @genres = Genre.all
       render :index
     end
   end
