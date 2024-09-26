23ae1c6b98212e2254c6faa797ad10c99bed29d6
Yuka511 <174612970+Yuka511@users.noreply.github.com>
2024-09-26 09:53:55 +0000

    修正

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,7 +1,7 @@
 class Admin::OrderDetailsController < ApplicationController
   before_action :authenticate_admin!
-
-
+  
+  
   def update
     @order_detail = OrderDetail.find(params[:id])
     @order_detail.update(making_status: params[:order_detail][:making_status])
@@ -12,25 +12,25 @@ class Admin::OrderDetailsController < ApplicationController
 
     if is_all_order_details_making_completed(order)
        order.update(status: 3)
-    end
-
-    flash[:notice] = "製作ステータスを変更しました。"
+    end  
+    
+    flash[:order] = "製作ステータスを変更しました。"
     redirect_to admin_order_path(@order_detail.order.id)
   end
-
+  
   private
-
+  
   def order_detail_params
     params.require(:order_detail).permit(:making_status)
   end
-
+  
   def is_all_order_details_making_completed(order)
     order.order_details.each do |order_detail|
       if order_detail.making_status != "finished"
-        return false
+        return false 
       end
     end
-    return true
-  end
+    return true 
+  end  
 
 end
